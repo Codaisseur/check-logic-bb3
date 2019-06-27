00a8a1ba90b023c5415e296eed0ba5863820557d
MimiMagusin <mimi@codaisseur.com>
2019-06-27 16:47:43 +0200

    setup jest.config

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ touch .stylelintIgnore;
 touch .stylelintrc.json;
 touch .eslintrc;
 touch .eslintignore;
+touch jest.config.js;
 echo 'check_logic\npackage.json\nyarn-error.log' >> .stylelintIgnore;
 echo '{ "rules": {"at-rule-no-unknown": true, "at-rule-semicolon-newline-after": "always", "block-no-empty": true, "block-opening-brace-newline-after": "always", "color-no-invalid-hex": true, "comment-no-empty": true, "declaration-block-no-duplicate-properties": true, "declaration-block-no-shorthand-property-overrides": true, "font-family-no-duplicate-names": true, "font-family-no-missing-generic-family-keyword": true, "no-duplicate-selectors": true, "no-empty-source": true, "no-extra-semicolons": true, "color-no-invalid-hex": true } }' >> .stylelintrc.json
 echo '{"extends": "standard"}' >> .eslintrc
 echo '/check_logic/' >> .eslintignore
-
+echo 'module.exports = {preset: "jest-puppeteer"}' >> jest.config.js;
