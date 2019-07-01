39a03d382731a6e6cf0ebdf8ce48b45f10362ac8
MimiMagusin <mimi@codaisseur.com>
2019-07-01 16:27:08 +0200

    Does I.AmAcceptingCOmments work? + fix typo

--- tests/codeceptjs/noAnonymousComments.test.js
+++ tests/codeceptjs/noAnonymousComments.test.js
@@ -7,7 +7,7 @@ Scenario('A comment without name is not displayed', (I) => {
   I.amOnPage(`file://${pwd}/pages/detail-page.html`);
   I.fillField('Message', 'IAmVeryUnique')
   I.click('Post your comment')
+  I.amAcceptingPopups()
   I.acceptPopup()
   I.dontSee('IAmVeryUnique', '.comment')
-  reloadPage()
 });
--- tests/codeceptjs/noEmptyComments.test.js
+++ tests/codeceptjs/noEmptyComments.test.js
@@ -7,6 +7,7 @@ Scenario('An empty comment is not displayed', (I) => {
   I.amOnPage(`file://${pwd}/pages/detail-page.html`);
   I.fillField('Name', 'MyUniqueName')
   I.click('Post your comment')
+  I.amAcceptingPopups()
+  I.acceptPopup()
   I.dontSee('MyUniqueName', '.comment')
-  reloadPage()
 });
